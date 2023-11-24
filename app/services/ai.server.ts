605b104fede53b277dd651caf6328eec9fc64b5a
Fidele K <itfidele@gmail.com>
2023-11-24 23:26:35 +0300

    Refactor AI server constructor to accept an API key parameter

--- app/services/ai.server.ts
+++ app/services/ai.server.ts
@@ -3,20 +3,26 @@ import { GoogleAuth } from "google-auth-library";
 import { GEN_AI_API_KEY } from "~/utils/env";
 
 
+
+
 export class AIServer {
   private client: TextServiceClient;
 
-  constructor() {
+  constructor(api_key:string) {
+    console.log('api_key=',api_key);
     this.client = new TextServiceClient({
-      authClient: new GoogleAuth().fromAPIKey(GEN_AI_API_KEY),
+
+      authClient: new GoogleAuth().fromAPIKey(api_key!='' ? api_key : GEN_AI_API_KEY),
     });
   }
 
   async generateText({topic,platform}:{topic:string,platform:string}): Promise<string> {
+    
+
     const content_ai:any = await this.client.generateText({
         model:'models/text-bison-001',
         prompt:{
-            text:`You are a post content creator. write a short ${platform} content about  "${topic}" and make it at least 200 words long. use plain text only. follow the instructions below: - no more than 200 words, - be creative`,
+            text: this._getPrompt(topic,platform)
         },
     });
 
@@ -25,5 +31,26 @@ export class AIServer {
     console.log(generatedText);    
 
     return generatedText;
+  }
+
+
+  private _getPrompt(topic:string,platform:string){
+    const p = platform.toLowerCase();
+    if(p == 'twitter'){
+        return `Write an informative Twitter post on the topic ${topic}. Also include relevant hashtags, NO MORE THAN 150 words`
+    }
+    else if (p == 'facebook'){
+        return `Write an engaging post related to the topic ${topic} for posting in a Facebook Group, NO MORE THAN 250 words`;
+    }
+    else if (p=='linkedin'){
+        return `Write a post related to the topic ${topic} for LinkedIn, NO MORE THAN 350 words. keep it interesting and story-like`;
+    }
+    else if( p == 'youtube'){
+        return `Write a script for a YouTube video on the topic ${topic}. NO MORE THAN 1000 words, NO LISTING. keep it interesting and story-like`;
+    }
+    else{
+        return  `You are a post content creator. write a short ${platform} content about  "${topic}" and make it at least 200 words long. use plain text only. follow the instructions below: - no more than 200 words, - be creative`;
+    }
+    
   }
 }
